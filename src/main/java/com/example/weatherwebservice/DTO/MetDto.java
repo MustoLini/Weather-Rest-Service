15d08deed3d8cc45b7d17015f003567eefb3aa6a
Johannes <danielssonjohannes@gmail.com>
2023-05-10 14:04:21 +0200

    Getting data from Met

--- src/main/java/com/example/weatherwebservice/DTO/MetDto.java
+++ src/main/java/com/example/weatherwebservice/DTO/MetDto.java
@@ -1,6 +1,17 @@
 package com.example.weatherwebservice.DTO;
 
-public class MetDto extends Dto{
+import com.example.weatherwebservice.SMHI.WeatherWebservice;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+public class MetDto extends Dto {
+    WebClient client = WebClient.create();
 
+    public MetDto() {
+        Mono<WeatherWebservice> mono = client
+                .get()
+                .uri("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.3110&lon=18.0300")
+                .retrieve()
+                .bodyToMono(WeatherWebservice.class);
+    }
 }

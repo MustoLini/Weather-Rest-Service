1e33a14666e73de1f8b25390a9eb7ae855500d8f
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-05-11 13:41:57 +0200

    Adding New Class to GettingInformationFromWebsite

---
+++ src/main/java/com/example/weatherwebservice/Service/GettingInformationFromWebsite.java
@@ -0,0 +1,9 @@
+package com.example.weatherwebservice.Service;
+
+import com.example.weatherwebservice.DTO.WeatherWebsiteDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GettingInformationFromWebsite{
+    @Autowired
+    WeatherWebsiteDTO weatherWebsiteDTO;
+}

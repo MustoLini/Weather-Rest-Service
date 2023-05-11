efdf114c196972d733ed24679463b6bf0cb55f1f
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-05-11 15:46:22 +0200

    Removing Class

--- src/main/java/com/example/weatherwebservice/Service/GettingInformationFromWebsite.java
+++
@@ -1,9 +0,0 @@
-package com.example.weatherwebservice.Service;
-
-import com.example.weatherwebservice.Entities.WeatherWebsiteEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class GettingInformationFromWebsite{
-    @Autowired
-    WeatherWebsiteEntity weatherWebsiteEntity;
-}

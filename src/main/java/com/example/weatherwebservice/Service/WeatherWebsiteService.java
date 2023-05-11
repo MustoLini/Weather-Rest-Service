afbeb686d135ad8b467271e9b8428cbc72c3bdaa
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-05-11 13:44:14 +0200

    Refactoring new WeatherWebsiteService

--- src/main/java/com/example/weatherwebservice/Service/WeatherWebsiteService.java
+++ src/main/java/com/example/weatherwebservice/Service/WeatherWebsiteService.java
@@ -2,27 +2,29 @@ package com.example.weatherwebservice.Service;
 
 import com.example.weatherwebservice.Client.MetClient;
 import com.example.weatherwebservice.Client.SmhiClient;
-import com.example.weatherwebservice.Client.WeatherWebsiteClient;
+import com.example.weatherwebservice.DTO.MetDTO;
+import com.example.weatherwebservice.DTO.SmhiDTO;
+import com.example.weatherwebservice.DTO.WeatherWebsiteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeatherWebsiteService {
     @Autowired
-    MetClient metClient;
+    MetDTO metDTO;
     @Autowired
-    SmhiClient smhiClient;
+    SmhiDTO smhiDTO;
 
-    public WeatherWebsiteClient getTheBestWeather() {
-        if (metClient.getTemperature() > smhiClient.getTemperature()) {
-            return metClient;
+    public WeatherWebsiteDTO getTheBestWeather() {
+        if (metDTO.getTemperature() > smhiDTO.getTemperature()) {
+            return metDTO;
         } else {
-            return smhiClient;
+            return smhiDTO;
         }
     }
 
     public String bestWeatherSource() {
-        WeatherWebsiteClient weather = getTheBestWeather();
+        WeatherWebsiteDTO weather = getTheBestWeather();
         if(weather.getClass().equals(SmhiClient.class)){
             return "SMHI";
         }

74d2b4014078197db7fec435095a13135f9f25e9
Ellinor <ellinorekmark@hotmail.com>
2023-05-11 15:08:20 +0200

    Added method to Rest Controller that returns best Weather.

--- src/main/java/com/example/weatherwebservice/RestController/WeatherWebsiteController.java
+++ src/main/java/com/example/weatherwebservice/RestController/WeatherWebsiteController.java
@@ -1,7 +1,10 @@
 package com.example.weatherwebservice.RestController;
 
+import com.example.weatherwebservice.Entities.WeatherWebsiteEntity;
 import com.example.weatherwebservice.Service.WeatherWebsiteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +12,11 @@ public class WeatherWebsiteController {
 
     @Autowired
     WeatherWebsiteService weatherWebsiteService;
+
+    @GetMapping("/rs/weather")
+    public ResponseEntity<WeatherWebsiteEntity> getBestWeather(){
+        return ResponseEntity.ok().body(weatherWebsiteService.getTheBestWeather());
+    }
 
 
 

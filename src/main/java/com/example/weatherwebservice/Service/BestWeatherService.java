4c82d7c59cf8dab1d2870820a1e26e9faef02c32
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-05-11 15:46:13 +0200

    Renaming Class name

--- src/main/java/com/example/weatherwebservice/Service/BestWeatherService.java
+++ src/main/java/com/example/weatherwebservice/Service/BestWeatherService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class WeatherWebsiteService {
+public class BestWeatherService {
     @Autowired
     MetEntity metEntity;
     @Autowired
